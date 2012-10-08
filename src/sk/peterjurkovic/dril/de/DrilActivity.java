65d4d9171891e4b799cff4684b312d48920f360a
Peter Jurkovič <petojurkovic@gmail.com>
2012-10-08 15:43:01 +0200

    Move to SDcard enabled, add word bug fixed

--- src/sk/peterjurkovic/dril/de/DrilActivity.java
+++ src/sk/peterjurkovic/dril/de/DrilActivity.java
@@ -20,7 +20,6 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
